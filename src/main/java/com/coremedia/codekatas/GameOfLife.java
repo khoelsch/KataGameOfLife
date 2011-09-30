00f81c642658b06ccc38d4e90a8d470b58a5c309
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-10-01 01:10:04 +0200

    Constructor checks

--- src/main/java/com/coremedia/codekatas/GameOfLife.java
+++ src/main/java/com/coremedia/codekatas/GameOfLife.java
@@ -6,4 +6,35 @@ import org.junit.Test;
  * Implementation of Conway's Game of Life as specified by <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataGameOfLife">Coding Dojo</a>.
  */
 public class GameOfLife {
+  // the edge length of the grid in X
+  private int dimX;
+
+  // the edge length of the grid in X
+  private int dimY;
+
+
+  public GameOfLife(int[][] initialGrid) {
+    validateGrid(initialGrid);
+
+    dimX = initialGrid.length;
+    dimY = initialGrid[0].length;
+  }
+
+  /**
+   * any edge length in X is legal
+   *
+   * length of first Array dictates the edge length in Y
+   *
+   * @param initialGrid
+   *
+   * @throws IllegalArgumentException
+   */
+  private static void validateGrid(int[][] initialGrid) throws IllegalArgumentException {
+    final int firstLengthY = initialGrid[0].length;
+    for (int[] yCol : initialGrid) {
+      if (yCol.length != firstLengthY) {
+        throw new IllegalArgumentException("All lines of the grid must have the same length!");
+      }
+    }
+  }
 }
--- src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
+++ src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
@@ -2,9 +2,14 @@ package com.coremedia.codekatas;
 
 import org.junit.Test;
 
-public class GameOfLifeTest {
-  @Test
-  public void properConstruction() {
+import com.coremedia.codekatas.GameOfLife;
 
+public class GameOfLifeTest {
+  @Test(expected = IllegalArgumentException.class)
+  public void properConstructionInvalidGrid() {
+     int[][] grid = {{0, 0, 0},
+                     {0, 0, 0},
+                     {0, 0}};
+    final GameOfLife gol = new GameOfLife(grid);
   }
 }
