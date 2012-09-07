2417ed67e3fcb78e2359a2fc2c98d37ba94bfbd9
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-09-07 16:25:23 +0200

    implement rule 1 with trivial field

--- src/main/java/com/coremedia/codekatas/GameOfLife.java
+++ src/main/java/com/coremedia/codekatas/GameOfLife.java
@@ -14,13 +14,66 @@ public class GameOfLife {
 
     field = new int[numRows][rowLength];
 
-    for (int i=0; i<numRows; ++i) {
+    for (int i = 0; i < numRows; ++i) {
       field[i] = Arrays.copyOf(initialState[i], rowLength);
     }
   }
 
-  public int cellAt(int xCoord, int yCoord) {
+  public void step() {
+    int [][] targetField = new int[field.length][field[0].length];
+    // scan the file row by row
+    // and set the new state of each cell
+    for (int row = 0; row < field.length; row++) {
+      for (int col = 0; col < field[row].length; col++) {
+        int currentCell = field[row][col];
+        int liveNeighbours = countLivingNeighbours(row, col);
+        // result
+        if (liveNeighbours < 2 && currentCell == 1) {
+          // die!
+          targetField[row][col] = 0;
+        }
+      }
+    }
+    field = targetField;
+  }
+
+  private int countLivingNeighbours(int row, int col) {
+    int liveNeighbours = 0;
+    // max eight neighbours
+    // check row by row
+    if (row - 1 > 0) {
+      // check neighbours in first row
+      if (col - 1 > 0 && cellAt(row - 1, col - 1) == 1) {
+        liveNeighbours++;
+      }
+      if (cellAt(row - 1, col) == 1) {
+        liveNeighbours++;
+      }
+      if (col + 1 < field[0].length && cellAt(row - 1, col + 1) == 1) {
+        liveNeighbours++;
+      }
+    }
+
+    // check neighbours in same row
+
+    if (row + 1 < field.length - 1) {
+      // check neighbours of row below
+       // check neighbours in first row
+      if (col - 1 > 0 && cellAt(row + 1, col - 1) == 1) {
+        liveNeighbours++;
+      }
+      if (cellAt(row + 1, col) == 1) {
+        liveNeighbours++;
+      }
+      if (col + 1 < field[0].length && cellAt(row + 1, col + 1) == 1) {
+        liveNeighbours++;
+      }
+    }
+    return liveNeighbours;
+  }
+
+  public int cellAt(int row, int col) {
     // Argument check ommited: It's a CodeKata!
-    return field[xCoord][yCoord];
+    return field[row][col];
   }
 }
--- src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
+++ src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
@@ -3,6 +3,8 @@ package com.coremedia.codekatas;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 public class GameOfLifeTest {
   @Test
   public void properConstruction() {
@@ -14,16 +16,34 @@ public class GameOfLifeTest {
 
     GameOfLife gol = new GameOfLife(input);
 
-    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(0, 0));
-    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(0, 1));
-    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(0, 2));
+    assertEquals("Contruction-Error!", 0, gol.cellAt(0, 0));
+    assertEquals("Contruction-Error!", 1, gol.cellAt(0, 1));
+    assertEquals("Contruction-Error!", 0, gol.cellAt(0, 2));
 
-    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(1, 0));
-    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(1, 1));
-    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(1, 2));
+    assertEquals("Contruction-Error!", 1, gol.cellAt(1, 0));
+    assertEquals("Contruction-Error!", 0, gol.cellAt(1, 1));
+    assertEquals("Contruction-Error!", 0, gol.cellAt(1, 2));
 
-    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(2, 0));
-    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(2, 1));
-    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(2, 2));
+    assertEquals("Contruction-Error!", 0, gol.cellAt(2, 0));
+    assertEquals("Contruction-Error!", 1, gol.cellAt(2, 1));
+    assertEquals("Contruction-Error!", 1, gol.cellAt(2, 2));
   }
+
+  @Test
+  public void testTrivialFieldDeadCell() {
+    int[][] trivialField = new int[1][1];
+    GameOfLife gol = new GameOfLife(trivialField);
+    gol.step();
+    // stays dead
+    assertEquals(0, gol.cellAt(0,0));
+  }
+
+  @Test
+    public void testTrivialFieldLiveCell() {
+      int[][] trivialField = {{1}};
+      GameOfLife gol = new GameOfLife(trivialField);
+      gol.step();
+      // stays dead
+      assertEquals(0, gol.cellAt(0,0));
+    }
 }
