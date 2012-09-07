a8d5f9787a68bb3a2ba1bf5b89ea411b1366e7af
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-09-07 15:49:29 +0200

    First testcase

--- src/main/java/com/coremedia/codekatas/GameOfLife.java
+++ src/main/java/com/coremedia/codekatas/GameOfLife.java
@@ -1,9 +1,26 @@
 package com.coremedia.codekatas;
 
-import org.junit.Test;
+import java.util.Arrays;
 
 /**
  * Implementation of Conway's Game of Life as specified by <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataGameOfLife">Coding Dojo</a>.
  */
 public class GameOfLife {
+  private int[][] field;
+
+  public GameOfLife(int[][] initialState) {
+    int numRows = initialState[0].length;
+    int rowLength = numRows; // Aussumption: We have a square field!
+
+    field = new int[numRows][rowLength];
+
+    for (int i=0; i<numRows; ++i) {
+      field[i] = Arrays.copyOf(initialState[i], rowLength);
+    }
+  }
+
+  public int cellAt(int xCoord, int yCoord) {
+    // Argument check ommited: It's a CodeKata!
+    return field[xCoord][yCoord];
+  }
 }
--- src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
+++ src/test/java/com/coremedia/codekatas/GameOfLifeTest.java
@@ -1,10 +1,29 @@
 package com.coremedia.codekatas;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 public class GameOfLifeTest {
   @Test
   public void properConstruction() {
+    int[][] input = {
+            {0, 1, 0},
+            {1, 0, 0},
+            {0, 1, 1}
+    };
 
+    GameOfLife gol = new GameOfLife(input);
+
+    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(0, 0));
+    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(0, 1));
+    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(0, 2));
+
+    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(1, 0));
+    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(1, 1));
+    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(1, 2));
+
+    Assert.assertEquals("Contruction-Error!", 0, gol.cellAt(2, 0));
+    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(2, 1));
+    Assert.assertEquals("Contruction-Error!", 1, gol.cellAt(2, 2));
   }
 }
